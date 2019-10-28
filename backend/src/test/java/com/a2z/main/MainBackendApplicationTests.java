e9cf4007c6b395af36ff2c7100f622d9419ff4c4
thbsie-portalprodsupport <thbsie_portalrefsupport@thbs.com>
2019-10-28 23:14:47 +0530

    backend framework build

---
+++ backend/src/test/java/com/a2z/main/MainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a2z.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
